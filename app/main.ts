2438f691316f09c3bb53d5bde5b8bba386f69a21
AmitThakkar <vigildbest@gmail.com>
2016-06-23 19:45:19 +0530

    Adding hello word.

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+/**
+ * Created by amitthakkar on 23/06/16.
+ */
+import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { AppComponent } from './app.component';
+bootstrap(AppComponent);
