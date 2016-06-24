3764588a584d13d41f7c29001b2dc140052b5fc8
AmitThakkar <vigildbest@gmail.com>
2016-06-24 08:37:35 +0530

    Adding example of few pipes.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,26 @@
 /**
  * Created by amitthakkar on 23/06/16.
  */
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <h3>Upper Case Pipe</h3>
+        <hr />
+        My Name in toString Form : <strong> {{ name }} </strong><br/>
+        My Name in Uppercase : <strong> {{ name | uppercase }} </strong>
+        <h3>Lower Case Pipe</h3>
+        <hr />
+        My Name in toString Form : <strong> {{ name }} </strong><br/>
+        My Name in Lowercase : <strong> {{ name | lowercase }} </strong>
+        <h3>date Pipe</h3>
+        <hr />
+        My DOB in toString Form : <strong> {{ birthday }} </strong><br/>
+        My DOM in Readable Form : <strong> {{ birthday | date }} </strong><br/>
+        My DOM in Custom Form : <strong> {{ birthday | date : 'dd/MM/yyyy' }} </strong>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+    name = 'Amit Thakkar';
+    birthday = new Date(1988, 9, 1); // October 1, 1988
+}
